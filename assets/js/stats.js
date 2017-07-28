cacf5bd39c72295c0a833dd24404ae580835b6c1
nwmn <jknewman@ucdavis.edu>
2017-07-27 19:05:57 -0700

    clean up / add statCounter

---
+++ assets/js/stats.js
@@ -0,0 +1,7 @@
+var sc_project=11406914;
+var sc_invisible=1;
+var sc_security="00dd814e";
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" + scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
